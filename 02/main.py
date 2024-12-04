05281038402d88e3370b82ea40dbcc4623a33779
PeterisS-alms <peteris.salms@grobinasskola.lv>
2024-12-04 14:25:07 +0200

    Pievienoju komentārus

--- 02/main.py
+++ 02/main.py
@@ -1,24 +1,24 @@
 """ Method 1 """
-
+# Parbauda skaitli 
 num = int(input("Give me a number to check: "))
-check = int(input("Give me a number to divide by: "))
+check = int(input("Give me a number to divide by: ")) # Dala ciparu
 
 if num % 4 == 0:
-    print(num, "is a multiple of 4")
+    print(num, "is a multiple of 4") # Termināli pārbauda vai ir vairāki 4
 elif num % 2 == 0:
-    print(num, "is an even number")
+    print(num, "is an even number") # Parbauda termināli vai tas dalas ar 2 un ir pāra skaitlis
 else:
-    print(num, "is an odd number")
+    print(num, "is an odd number") # Pārbauda termināli vai tas dalās ar 2 un ir nepāra skaitlis
 
 if num % check == 0:
-    print(num, "divides evenly by", check)
+    print(num, "divides evenly by", check) # Parbauda Termināli vai tas dalās vienlīdzīgi
 else:
-    print(num, "doesn't divide evenly by", check)
+    print(num, "doesn't divide evenly by", check) # Pārbauda termināli vai tas nedalās lidzigi
 
 """ Method 2 """
-num = int(input("Enter a number: "))
+num = int(input("Enter a number: ")) # ievada skaitli un dala ar 2
 mod = num % 2
 if mod > 0:
-    print("You picked an odd number.")
+    print("You picked an odd number.") # Parāda vai tu izvēlejies nepāra skaitli dalot tavu skaitli ar 2
 else:
-    print("You picked an even number.")
+    print("You picked an even number.") # Parāda vai tu izvēlejies pāra skaitli dalot tavu skailti ar 2
--- 03/main.py
+++ 03/main.py
@@ -1,11 +1,11 @@
-a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89] # parāda ka a ir vienāds ar šiem skaitļiem
 
-num = int(input("Choose a number: "))
+num = int(input("Choose a number: ")) # Prasa tev ievadīt skaitli
 
-new_list = []
+new_list = [] # izveido jaunu sarakstu
 
 for i in a:
     if i < num:
-        new_list.append(i)
+        new_list.append(i) # Pievieno jaunu sarakstu
 
-print(new_list)
+print(new_list) # printē jaunu sarakstu
