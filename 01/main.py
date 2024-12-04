7380dfbc11cfd4a9b8b37ff89baa7fa81848ce53
PeterisS-alms <peteris.salms@grobinasskola.lv>
2024-12-04 14:14:33 +0200

    pieliku komentarus

--- 01/main.py
+++ 01/main.py
@@ -1,11 +1,12 @@
+# Ievada vārdu
 """ Insert Name """
 name = input("Whats your name: ")
-
+# Ievada skaitlisku vecumu
 """ Insert age. Also convert age into an Integer value """
 age = int(input("How old are you: "))
-
+# Aprēķinājumi ar iegūto vecumu
 """ Do the math """
 year = str((2018 - age)+100)
-
+# Parāda terminalī kad tev bus 100 gadi
 """ Print the result """
 print(name + " will be 100 years old in the year " + year)
